966190b720d3a2b2618164c175c0d737d6872039
kaunghtutthar <kaunghtutthar63@gmail.com>
2026-01-10 22:07:05 +0630

    Run error

--- run.py
+++ run.py
@@ -1,12 +1,3 @@
 from app import create_app
 
-app = None
-
-try:
-    app = create_app()
-except Exception as e:
-    print("APP FAILED TO START:", e)
-    raise
-
-if __name__ == "__main__":
-    app.run(debug=True)
+app = create_app()
