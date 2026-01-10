7a8173442c05aa67bea25aca4aa80b94b4714b5c
kaunghtutthar <kaunghtutthar63@gmail.com>
2026-01-10 21:55:22 +0630

    Expose startup error

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 from app import create_app
 
-app = create_app()
+app = None
+
+try:
+    app = create_app()
+except Exception as e:
+    print("APP FAILED TO START:", e)
+    raise
 
 if __name__ == "__main__":
     app.run(debug=True)
